bf7c83b4a14ff84dc60c2f60ea6da3add8ff4d21
Albert Yue <albert.s.yue@gmail.com>
2019-03-15 16:01:22 -0400

    switch to grayscale and 1 output layer

--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/flags --name flags_pix2pix --model pix2pix --netG unet_128 --input_nc 9 --direction AtoB --lambda_L1 100 --dataset_mode unaligned_hdf5 --norm batch --pool_size 0 --gpu_ids -1
+python train.py --dataroot ./datasets/flags --name flags_pix2pix --model pix2pix --netG unet_128 --input_nc 9 --output_nc 1 --direction AtoB --lambda_L1 100 --dataset_mode unaligned_hdf5 --norm batch --pool_size 0 --gpu_ids -1
